fcf10bf5bb52c3c02e6a8f5e37cb72945f27e574
jihyeon <jihyeon33@gmail.com>
2023-07-24 01:29:23 +0900

    메인화면이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import styles from "@/styles/page.module.scss";
+import Link from "next/link";
+import mainImage from "@/public/image/main.jpg";
 /* Prop =========================================================== */
 type Prop = {};
 type SkillProp = {
@@ -6,5 +9,32 @@ type SkillProp = {
   icon: any;
 };
 export default function Home() {
-  return <main>Home 페이지 입니다 </main>;
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.main}>
+        <div className={styles.main_content}>
+          <div className={styles.main_tit}>
+            Web designer
+            <br /> Publisher
+          </div>
+          <div className={styles.main_txt}>
+            <p>웹디자이너, 웹 퍼블리셔 홍지현입니다.</p>
+            <p>
+              최적의 사용자 경험을 고려한 트랜디한 디자인으로 상상을 실현해 낼
+              수 있도록 항상 최선을 다하고 있습니다.
+            </p>
+            <p>
+              저는 언제나 배움의 자세를 가지고 새로운 것을 배우려는 의지를
+              가지고 있습니다. 저에 대해서 궁금하시다면 아래의 메일 주소로
+              언제든 연락 주시기 바랍니다.
+            </p>
+          </div>
+          <div className={styles.contact}>jihyeon33@gmail.com</div>
+        </div>
+        <div className={styles.main_img}>
+          <Image src={mainImage} alt="jihyeon" className={styles.img} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,46 @@
+"use client";
+
 import Link from "next/link";
 import styles from "@/components/Header.module.scss";
+import Image from "next/image";
+import Logo from "@/public/logo.svg";
+import React from "react";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
-    <ul className={styles.navibar}>
-      <li className={styles.navi_item}>
-        <Link href="/">home</Link>
-      </li>
-      <li className={styles.navi_item}>
-        <Link href="/projects">project</Link>
-      </li>
-      <li className={styles.navi_item}>
-        <Link href="/study">study</Link>
-      </li>
-    </ul>
+    <div className={styles.header}>
+      <div className={styles.navi_wrap}>
+        <h1>
+          <Link href={"/"}>
+            <Image src={Logo} alt="지현포트폴리오" className={styles.logo} />
+          </Link>
+        </h1>
+        <ul className={styles.navibar}>
+          <li className={styles.navi_item}>
+            <Link href={"/"} className={pathname === "/" ? styles.active : ""}>
+              home
+            </Link>
+          </li>
+          <li className={styles.navi_item}>
+            <Link
+              href={"/projects"}
+              className={pathname === "/projects" ? styles.active : ""}
+            >
+              project
+            </Link>
+          </li>
+          <li className={styles.navi_item}>
+            <Link
+              href={"/study"}
+              className={pathname === "/study" ? styles.active : ""}
+            >
+              study
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
